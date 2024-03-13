bbee8ad798465201abd80035f7092e6a0c5fc388
Harshit-Jajoria <122677014+Harshit-Jajoria@users.noreply.github.com>
2024-03-14 00:12:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send(`Hello from backend index.js and port number ${port} hello 3 auto`);
+  res.send(`Hello from backend index.js and port number ${port} hello 4 auto`);
 });
 
 //routes
